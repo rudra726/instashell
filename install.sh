a353b5986ad7508d548907bcab22036b03290881
rudra726 <86190721+rudra726@users.noreply.github.com>
2021-06-20 17:33:15 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,4 +21,7 @@ checkroot
 (trap '' SIGINT SIGTSTP && command -v curl > /dev/null 2>&1 || { printf >&2  "\e[1;92mInstalling cURL, please wait...\n\e[0m"; apt-get update > /dev/null && apt-get -y install curl > /dev/null || printf "\e[1;91mCurl Not installed.\n\e[0m"; }) & wait $!
 
 printf "\e[1;92mAll Requires are installed!\n\e[0m"
+cd instashell/
 
+chmod 775 instashell.sh
+chmod 775 install.sh
